82e2d3d0772980542b3de824c3614b8e01049836
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 19:44:08 -0300

    Teste do uso de variável com o tipo de dados char em Java.

---
+++ src/TesteVariavelChar.java
@@ -0,0 +1,9 @@
+public class TesteVariavelChar {
+
+    public static void main(String[] args) {
+        char caractereUnicode = '\u0041';
+        char caractereNormal = 'A';
+
+        System.out.printf("Os caracteres: %c e %c são iguais", caractereUnicode, caractereNormal);
+    }
+}
